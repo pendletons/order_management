4d3a7240be3b11b43b9831bef87a502fca89be50
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2016-03-25 08:54:49 +0000

    initial Rails setup
    include rspec, cucumber, simplecov, and database_cleaner

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.5'
+gem 'pg', '~> 0.15'
+
+gem 'active_model_serializers'
+
+group :development, :test do
+  gem 'byebug'
+  gem 'rspec-rails'
+end
+
+group :development do
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+end
+
+group :test do
+  gem 'cucumber-rails', require: false
+  gem 'database_cleaner'
+  gem 'simplecov', require: false
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,32 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'database_cleaner'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+end
